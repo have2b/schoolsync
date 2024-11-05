7a4bf30b915dfd4fe3e9d6c42bbd745126223de1
tlongizunique <thanhlongvu156@gmail.com>
2024-11-05 14:31:46 +0700

    refactor: remove noti icon and message icon

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -7,19 +7,15 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components';
-import { BellIcon, ChevronDownIcon, DoorOpenIcon, MailIcon, UserIcon } from 'lucide-react';
+import { ChevronDownIcon, DoorOpenIcon, UserIcon } from 'lucide-react';
 import { getTranslations } from 'next-intl/server';
 import { LangSwitch } from './LangSwitch';
 
 export const Header = async () => {
   const t = await getTranslations('Header');
   return (
-    <header className="flex w-full items-center justify-end gap-2 p-4 shadow-md">
+    <header className="flex w-full items-center justify-end gap-4 p-6 shadow-md">
       <LangSwitch />
-      <div className="me-4 flex items-center justify-center gap-3 border-e-2 border-black px-4 py-1">
-        <MailIcon className="size-5" />
-        <BellIcon className="size-5" />
-      </div>
       <DropdownMenu>
         <DropdownMenuTrigger>
           <div className="flex items-center justify-center gap-1">
@@ -27,19 +23,19 @@ export const Header = async () => {
               <AvatarImage src="https://github.com/have2b.png" />
               <AvatarFallback>AV</AvatarFallback>
             </Avatar>
-            <ChevronDownIcon className="h-4 w-4" />
+            <ChevronDownIcon className="size-5" />
           </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent>
           <DropdownMenuItem>
             <div className="flex items-center justify-center gap-2">
-              <UserIcon className="h-4 w-4" />
+              <UserIcon className="size-5" />
               <span className="font-medium">{t('profile')}</span>
             </div>
           </DropdownMenuItem>
           <DropdownMenuItem>
             <div className="flex items-center justify-center gap-2 text-red-600">
-              <DoorOpenIcon className="h-4 w-4" />
+              <DoorOpenIcon className="size-5" />
               <span className="font-medium">{t('logout')}</span>
             </div>
           </DropdownMenuItem>
