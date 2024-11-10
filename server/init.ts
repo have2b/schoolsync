8399e770f45dd8a5673f6d56e4edabc7a802b046
tlongizunique <thanhlongvu156@gmail.com>
2024-11-10 13:04:47 +0700

    feat: update data fake for testing

--- server/init.ts
+++ server/init.ts
@@ -1,59 +1,44 @@
 import prisma from '@/prisma';
+import { faker } from '@faker-js/faker';
 import argon2 from 'argon2';
 
 const accountData = await prisma.account.findMany({});
 const passwordHash = await argon2.hash('password');
 
 if (accountData.length === 0) {
+  await prisma.account.create({
+    data: {
+      username: 'admin',
+      email: 'admin@example.com',
+      password: passwordHash,
+      name: 'Admin',
+      role: 'Admin',
+      isFirstLogin: false,
+    },
+  });
+  const fakeAccounts = Array.from({ length: 1000 }).map(() => ({
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    password: passwordHash,
+    name: faker.person.fullName(),
+    role: faker.helpers.arrayElement(['Teacher', 'Student']),
+    isFirstLogin: faker.datatype.boolean(),
+  }));
+
   await prisma.account.createMany({
-    data: [
-      {
-        username: 'admin',
-        email: 'admin@example.com',
-        password: passwordHash,
-        name: 'Admin',
-        role: 'Admin',
-        isFirstLogin: false,
-      },
-      {
-        username: 'teacher',
-        email: 'teacher@example.com',
-        password: passwordHash,
-        name: 'Teacher',
-        role: 'Teacher',
-        isFirstLogin: false,
-      },
-      {
-        username: 'student',
-        email: 'student@example.com',
-        password: passwordHash,
-        name: 'Student',
-        role: 'Student',
-        isFirstLogin: false,
-      },
-    ],
+    data: fakeAccounts,
   });
 }
 
 const departmentData = await prisma.department.findMany({});
 if (departmentData.length === 0) {
+  const fakeDepartments = Array.from({ length: 1000 }).map((_, index) => ({
+    code: `K00${index + 1}`,
+    name: `Department ${index + 1}`,
+    detail: faker.lorem.sentence(),
+  }));
+
   await prisma.department.createMany({
-    data: [
-      {
-        code: 'K001',
-        name: 'Department 1',
-        detail: 'Department 1 detail',
-      },
-      {
-        code: 'K002',
-        name: 'Department 2',
-        detail: 'Department 2 detail',
-      },
-      {
-        code: 'K003',
-        name: 'Department 3',
-        detail: 'Department 3 detail',
-      },
-    ],
+    data: fakeDepartments,
   });
 }
