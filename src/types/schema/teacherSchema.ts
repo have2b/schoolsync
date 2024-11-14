bb39e757fe6b74b38930f916c9c993be0303266f
tlongizunique <thanhlongvu156@gmail.com>
2024-11-15 01:37:19 +0700

    feat: update teacher with ui

--- src/types/schema/teacherSchema.ts
+++ src/types/schema/teacherSchema.ts
@@ -13,7 +13,10 @@ const createTeacherSchema = z.object({
 const updateTeacherSchema = z.object({
   code: z.string(),
   name: z.string().min(2).max(50),
-  detail: z.string().max(50).optional(),
+  degree: z.string().max(50).optional(),
+  major: z.string().max(50).optional(),
+  departmentId: z.string().max(50).optional(),
+  avatar: z.string().max(50).optional(),
 });
 
 export { createTeacherSchema, updateTeacherSchema };
