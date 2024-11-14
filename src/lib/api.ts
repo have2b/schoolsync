d0cb6ef06d81a1cb1018279705a508faf1f0ab83
tlongizunique <thanhlongvu156@gmail.com>
2024-11-15 02:47:59 +0700

    cicd: fix install dep

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.API_URL || 'http://localhost:3000/api'; // Set the base URL for all requests
+axios.defaults.baseURL = '/api'; // Set the base URL for all requests
 
 const api = axios;
 
