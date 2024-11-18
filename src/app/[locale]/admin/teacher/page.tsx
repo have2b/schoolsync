df5673c17d810c64d3bef37a1acc6a9a85266e61
longvt1 <long.vuthanh1@nashtechglobal.com>
2024-11-18 16:41:54 +0700

    feat: export data for teacher list

--- src/app/[locale]/admin/teacher/page.tsx
+++ src/app/[locale]/admin/teacher/page.tsx
@@ -3,8 +3,47 @@
 import { DataTable } from '@/components';
 import { useCrud } from '@/hooks';
 import { teacherColumn } from '@/types';
+import { Teacher } from '@prisma/client';
+import { Row } from '@tanstack/react-table';
+import { format } from 'date-fns';
+import { useTranslations } from 'next-intl';
+import * as XLSX from 'xlsx';
 
 const AdminDepartment = () => {
+  const t = useTranslations();
+
+  const exportToExcel = (rows: Row<Teacher>[]) => {
+    const rowData = rows.map((row) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const item = row.original as Record<string, any>;
+      // Create a new object with only the columns we want, with custom names
+      return {
+        [t('teacher.fields.code.label')]: item.code,
+        [t('teacher.fields.name.label')]: item.name,
+        [t('teacher.fields.degree.label')]: t('enum.degree.' + item.degree.toLowerCase()),
+        [t('teacher.fields.major.label')]: item.major,
+      };
+    });
+    const worksheet = XLSX.utils.json_to_sheet(rowData);
+
+    // Optional: Adjust column widths
+    const columnWidths = Object.keys(rowData[0] || {}).reduce(
+      (acc, key) => {
+        acc[key] = { wch: Math.max(key.length, 15) }; // minimum width of 15 characters
+        return acc;
+      },
+      {} as { [key: string]: { wch: number } }
+    );
+    worksheet['!cols'] = Object.values(columnWidths);
+
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Data');
+
+    // Generate filename with date
+    const date = format(new Date(), 'yyyy-MM-dd');
+    XLSX.writeFile(workbook, `student-${date}.xlsx`);
+  };
+
   const { useList } = useCrud({ modelName: 'teacher' });
   const { data, isLoading, error } = useList();
 
@@ -25,6 +64,7 @@ const AdminDepartment = () => {
       data={activeData ?? []}
       searchableColumns={['code', 'name', 'degree', 'major']}
       modelName="teacher"
+      exportToExcel={exportToExcel}
     />
   );
 };
